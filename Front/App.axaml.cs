4fee8ea084258e81a4b868f184d76818c899e7eb
Uriel_Maceri <URI_maceri@hotmail.com>
2026-07-13 13:19:20 -0300

    Frontend - Initial Setup
    Added migration on startup and started modeling MainWindow. There's still a lot to do

--- Front/App.axaml.cs
+++ Front/App.axaml.cs
@@ -1,11 +1,16 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Back.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Front;
 
 public partial class App : Application
 {
+    public static IServiceProvider Services { get; private set; } = null!;
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -13,6 +18,12 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        var services = new ServiceCollection();
+        services.AddAtlasBackServices();
+        Services = services.BuildServiceProvider();
+        
+        Services.GetRequiredService<AtlasDbContext>();
+        
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow();
